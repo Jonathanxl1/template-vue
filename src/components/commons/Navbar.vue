c5904c0ae7af4eaf2a27a2d2e924d5de2956f902
Jonathan A <jonathanxl1@hotmail.com>
2024-10-16 15:49:29 -0500

    style: Define behavior desktop and mobile - Added router link

--- src/components/commons/Navbar.vue
+++ src/components/commons/Navbar.vue
@@ -1,8 +1,10 @@
 <template>
   <nav class="w-full h-12 flex justify-between p-4 bg-red-500">
-    <div>Logo</div>
-    <div class="sm:hidden lg:block">Links</div>
-    <div class="sm:block lg:hidden">Links Mobile</div>
+    <RouterLink to="/">
+      <div>Logo</div>
+    </RouterLink>
+    <div class="hidden md:block">Links</div>
+    <div class="block md:hidden">Links Mobile</div>
   </nav>
 </template>
 
