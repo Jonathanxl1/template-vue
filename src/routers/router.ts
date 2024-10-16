f3a39768938ec08c59f079de8baf09ff92bb9a02
Jonathan A <jonathanxl1@hotmail.com>
2024-10-16 15:50:59 -0500

    fix: Solved sticky page WHEN change page history mode

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,6 +1,20 @@
-import { createMemoryHistory, createRouter, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import Home from "../pages/home.vue";
+import Admin from "../pages/admin.vue";
+import Cart from "../pages/cart.vue";
 
-const routes: Array<RouteRecordRaw> = [{ path: "/", component: Home }];
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    component: Home,
+    name: "Shop",
+  },
+  { path: "/admin", component: Admin, name: "Administration" },
+  { path: "/cart", component: Cart, name: "Cart" },
+];
 
-export const router = createRouter({ history: createMemoryHistory(), routes });
+export const router = createRouter({ history: createWebHistory(), routes });
+
+router.beforeEach((to) => {
+  console.log(to.name);
+});
