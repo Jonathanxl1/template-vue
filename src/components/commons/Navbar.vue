7438cf1aef76b1d814871f27bb1de2c0d918edee
Jonathan A <jonathanxl1@hotmail.com>
2024-10-16 14:04:44 -0500

    feat:Default navbar component

---
+++ src/components/commons/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <nav class="w-full h-12 flex justify-between p-4 bg-red-500">
+    <div>Logo</div>
+    <div class="sm:hidden lg:block">Links</div>
+    <div class="sm:block lg:hidden">Links Mobile</div>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
