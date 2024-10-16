eceddd13d2d012cddb4bc1363f313f33a12919b9
Jonathan A <jonathanxl1@hotmail.com>
2024-10-16 14:06:34 -0500

    feat:Component current state page

---
+++ src/components/commons/PageState.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="w-full min-h-[200px] bg-blue-400 flex justify-center items-center">
+    <span>{{ currentPage }}</span>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { onMounted, ref } from "vue";
+  import { useRoute } from "vue-router";
+
+  const router = useRoute();
+  const currentPage = ref("");
+
+  onMounted(() => {
+    currentPage.value = String(router.name);
+  });
+</script>
+
+<style scoped></style>
