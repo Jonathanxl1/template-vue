c380677c8f56fd5f65e4eaff4844c328f6438922
Jonathan A <jonathanxl1@hotmail.com>
2024-10-16 15:27:50 -0500

    refactor: clean code

--- src/components/commons/PageState.vue
+++ src/components/commons/PageState.vue
@@ -1,20 +1,10 @@
 <template>
   <div
     class="w-full min-h-[200px] bg-blue-400 flex justify-center items-center">
-    <span>{{ currentPage }}</span>
+    <span>{{ $route.name }}</span>
   </div>
 </template>
 
-<script setup lang="ts">
-  import { onMounted, ref } from "vue";
-  import { useRoute } from "vue-router";
-
-  const router = useRoute();
-  const currentPage = ref("");
-
-  onMounted(() => {
-    currentPage.value = String(router.name);
-  });
-</script>
+<script setup lang="ts"></script>
 
 <style scoped></style>
