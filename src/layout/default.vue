e4d9a415b915d047bef98edab129fca54ce1c2f0
Jonathan A <jonathanxl1@hotmail.com>
2024-10-18 13:59:42 -0500

    refactor: Added component modal to default layout

--- src/layout/default.vue
+++ src/layout/default.vue
@@ -5,6 +5,7 @@
       <PageState />
       <RouterView></RouterView>
     </main>
+    <Modal></Modal>
   </div>
 </template>
 
@@ -12,6 +13,5 @@
   import { RouterView } from "vue-router";
   import Navbar from "../components/commons/Navbar.vue";
   import PageState from "../components/commons/PageState.vue";
+  import Modal from "../components/commons/Modal.vue";
 </script>
-
-<style scoped></style>
