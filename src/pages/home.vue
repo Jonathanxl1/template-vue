4ba76a35500173cceb983687061e12c99e904125
Jonathan A <jonathanxl1@hotmail.com>
2024-10-18 14:00:43 -0500

    refactor:Added component Cardview develop

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>Home</div>
+  <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 p-4">
+    <CardView></CardView>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { onMounted } from "vue";
+  import CardView from "../components/Cart/CardView.vue";
+  import { useProductsStore } from "../store/products.store";
+
+  const store = useProductsStore();
+
+  onMounted(() => {
+    store.getProducts();
+  });
+</script>
 
 <style scoped></style>
