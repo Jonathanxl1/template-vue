c5ca9a3c27b0dc29ab25d644b9a0f8f5a33a78a7
Jonathan A. (Ubuntu) <jonathanxl1@hotmail.com>
2025-02-20 17:11:23 -0500

    refactor: change path components and include
    - Include Footer component

--- src/layout/default.vue
+++ src/layout/default.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="w-full lg:min-h-[400px] sm:min-h-screen bg-redes">
+  <div class="w-full lg:min-h-[400px] sm:min-h-screen bg-slate-200">
     <Navbar />
     <main>
       <PageState />
       <RouterView></RouterView>
+      <Footer></Footer>
     </main>
     <Modal></Modal>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { RouterView } from "vue-router";
-  import Navbar from "../components/commons/Navbar.vue";
-  import PageState from "../components/commons/PageState.vue";
-  import Modal from "../components/commons/Modal.vue";
+import { RouterView } from "vue-router";
+import Navbar from "@/components/commons/Navbar.vue";
+import PageState from "@/components/commons/PageState.vue";
+import Modal from "@/components/commons/Modal.vue";
+import Footer from "@/components/commons/Footer.vue";
 </script>
